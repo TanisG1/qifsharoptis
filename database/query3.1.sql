b999e426dd11b01a309c99876b41f334a858c2c8
TanisG1 <106876589+TanisG1@users.noreply.github.com>
2022-06-08 21:16:34 +0300

    ye

--- database/query3.1.sql
+++ database/query3.1.sql
@@ -4,7 +4,7 @@ select p.project_name as title
 from projects p 
 inner join manage m on m.project_name=p.project_name
 inner join executives e on m.id = e.executive_id 
-where e.executive_id like '%' and p.duration > 0 and ((p.start_date < current_date())  and (p.end_date > current_date()));
+where e.executive_id=? and p.duration=? and ((p.start_date < current_date())  and (p.end_date > current_date()));
 
 select researcher_name from (
 (select concat(r.researcher_last_name," ", r.researcher_first_name) as researcher_name, p.project_name 
